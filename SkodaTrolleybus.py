b244b2b9d433e16ccd3989cf2fe8f1ba93936120
Aleesia <48089816+Aleesia@users.noreply.github.com>
2022-02-09 01:35:06 +0200

    Delete SkodaTrolleybus.py

--- SkodaTrolleybus.py
+++
@@ -1,9 +0,0 @@
-from Trolleybus import Trolleybus
-
-
-class SkodaTrolleybus(Trolleybus):
-    def __init__(self):
-        super().__init__(6800000, 13)
-
-    def run_by_contact_network(self):
-        print("run_by_contact_network: Skoda Trolleybus")
